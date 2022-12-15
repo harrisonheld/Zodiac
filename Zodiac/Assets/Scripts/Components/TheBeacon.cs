89ff01fa7b56342ddd15972101c3bb0ed3ef45ff
Harrison Held <harrydheld@gmail.com>
2022-12-14 19:16:02 -0600

    Polymorphic Event Handling on Components

--- Zodiac/Assets/Scripts/Components/TheBeacon.cs
+++ Zodiac/Assets/Scripts/Components/TheBeacon.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class TheBeacon : ZodiacComponent
 {
-    public override void HandleEvent(ZodiacEvent e)
-    {
-        
-    }
-    public override void HandleEvent(PickedUpEvent e)
+    public override bool HandleEvent(PickedUpEvent e)
     {
         if(e.pickerUpper == GameManager.Instance.ThePlayer)
         {
             AlertMenu.Instance.ShowText("A NEW HAND TOUCHES THE BEACON.");
         }
+        return true;
     }
 }
--- Zodiac/Assets/Scripts/Components/ZodiacComponent.cs
+++ Zodiac/Assets/Scripts/Components/ZodiacComponent.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
-
 public abstract class ZodiacComponent : MonoBehaviour
 {
-    public virtual void HandleEvent(ZodiacEvent e) { }
-    public virtual void HandleEvent(PickedUpEvent e) { }
+    public virtual void HandleEvent(ZodiacEvent e)
+    {
+        // this will call the appropriate HandleEvent for the event type
+        dynamic casted = e;
+        HandleEvent(casted);
+    }
+    public virtual bool HandleEvent(PickedUpEvent e) { return false; }
 }
