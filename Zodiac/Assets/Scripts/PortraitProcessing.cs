b027cdb722f65d3039d264023de14f4013d096ad
Harrison Held <harrydheld@gmail.com>
2023-08-02 22:32:51 -0500

    Made portrait filters a pre-processing step.

---
+++ Zodiac/Assets/Scripts/PortraitProcessing.cs
@@ -0,0 +1,136 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PortraitProcessing : MonoBehaviour
+{
+    // The folder containing the original portraits
+    public string originalPortraitsFolderPath = "Assets/Textures/UnfilteredPortraits/";
+    // The folder to save the filtered portraits
+    public string filteredPortraitsFolderPath = "Assets/Resources/Portraits/";
+
+    public int outputWidth = 128;
+    public int outputHeight = 128;
+
+    // Function to apply the filter and save the filtered portraits
+    public void PreprocessAllPortraits()
+    {
+        // Get all the texture files from the original portraits folder
+        string[] originalPortraitFiles = System.IO.Directory.GetFiles(originalPortraitsFolderPath, "*.png");
+
+        foreach (string filePath in originalPortraitFiles)
+        {
+            // Load the original portrait texture
+            Texture2D originalPortrait = LoadTextureFromFile(filePath);
+
+            // scale portrait
+            int widthScaleFactor = originalPortrait.width / outputWidth;
+            int heightScaleFactor = originalPortrait.height / outputHeight;
+            Texture2D filteredPortrait = new Texture2D(outputWidth, outputHeight, TextureFormat.ARGB32, false);
+            filteredPortrait.filterMode = FilterMode.Point; // for pixelated look
+            for (int y = 0; y < outputHeight; y++)
+            {
+                for (int x = 0; x < outputWidth; x++)
+                {
+                    int originalX = Mathf.RoundToInt(x * widthScaleFactor);
+                    int originalY = Mathf.RoundToInt(y * heightScaleFactor);
+
+                    Color color = originalPortrait.GetPixel(originalX, originalY);
+
+                    filteredPortrait.SetPixel(x, y, color);
+                }
+            }
+
+            // apply 4tone monochrome
+            Color[] pixels = filteredPortrait.GetPixels();
+            Color[] newPixels = new Color[pixels.Length];
+
+            Color black = new Color(0, 0, 0);
+            Color darkGray = new Color(0.25f, 0.25f, 0.25f);
+            Color lightGray = new Color(0.75f, 0.75f, 0.75f);
+            Color white = new Color(1, 1, 1);
+
+            float threshold = 0.25f;
+
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                float grayscale = pixels[i].grayscale;
+
+                if (grayscale < threshold)
+                    newPixels[i] = black;
+                else if (grayscale < 2 * threshold)
+                    newPixels[i] = darkGray;
+                else if (grayscale < 3 * threshold)
+                    newPixels[i] = lightGray;
+                else
+                    newPixels[i] = white;
+            }
+            filteredPortrait.SetPixels(newPixels);
+
+            // Floyd-Steinberg dithering
+            for (int y = 0; y < filteredPortrait.height; y++)
+            {
+                for (int x = 0; x < filteredPortrait.width; x++)
+                {
+                    Color oldColor = filteredPortrait.GetPixel(x, y);
+                    Color newColor = newPixels[y * filteredPortrait.width + x];
+                    filteredPortrait.SetPixel(x, y, newColor);
+                    Color error = oldColor - newColor;
+
+                    if (x < filteredPortrait.width - 1)
+                    {
+                        filteredPortrait.SetPixel(x + 1, y, filteredPortrait.GetPixel(x + 1, y) + error * 7 / 16f);
+                    }
+
+                    if (x > 0 && y < filteredPortrait.height - 1)
+                    {
+                        filteredPortrait.SetPixel(x - 1, y + 1, filteredPortrait.GetPixel(x - 1, y + 1) + error * 3 / 16f);
+                    }
+
+                    if (y < filteredPortrait.height - 1)
+                    {
+                        filteredPortrait.SetPixel(x, y + 1, filteredPortrait.GetPixel(x, y + 1) + error * 5 / 16f);
+                    }
+
+                    if (x < filteredPortrait.width - 1 && y < filteredPortrait.height - 1)
+                    {
+                        filteredPortrait.SetPixel(x + 1, y + 1, filteredPortrait.GetPixel(x + 1, y + 1) + error * 1 / 16f);
+                    }
+                }
+            }
+
+            filteredPortrait.Apply();
+
+            // Save the filtered portrait as a PNG file in the filtered portraits folder
+            string fileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            string outputPath = System.IO.Path.Combine(filteredPortraitsFolderPath, $"{fileName}.png");
+            byte[] bytes = filteredPortrait.EncodeToPNG();
+            System.IO.File.WriteAllBytes(outputPath, bytes);
+        }
+    }
+
+    // Function to load a texture from a file
+    private Texture2D LoadTextureFromFile(string filePath)
+    {
+        byte[] fileData = System.IO.File.ReadAllBytes(filePath);
+        Texture2D texture = new Texture2D(2, 2); // Provide initial dimensions
+        texture.LoadImage(fileData);
+        return texture;
+    }
+}
+
+
+[CustomEditor(typeof(PortraitProcessing))]
+public class PortraitPreprocessingEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Preprocess Portraits"))
+        {
+            PortraitProcessing portraitProcessing = (PortraitProcessing)target;
+            portraitProcessing.PreprocessAllPortraits();
+        }
+    }
+}
--- Zodiac/Assets/Scripts/UI/ConversationMenu.cs
+++ Zodiac/Assets/Scripts/UI/ConversationMenu.cs
@@ -55,103 +55,10 @@ namespace UI
 
             if(!string.IsNullOrEmpty(_currentNode.NpcPortrait))
             {
-                // TODO: move all this shit somewhere else and don't do it on the fly
-                Texture2D originalPortrait = Resources.Load<Texture2D>($"Portraits/{_currentNode.NpcPortrait}");
-
-                // scale portrait down
-                int SCALED_WIDTH = 128;
-                int SCALED_HEIGHT = 128;
-                int widthScaleFactor = originalPortrait.width / SCALED_WIDTH;
-                int heightScaleFactor = originalPortrait.height / SCALED_HEIGHT;
-                Texture2D filteredPortrait = new Texture2D(SCALED_WIDTH, SCALED_HEIGHT, TextureFormat.ARGB32, false);
-                filteredPortrait.filterMode = FilterMode.Point; // for pixelated look
-                for (int y = 0; y < SCALED_HEIGHT; y++)
-                {
-                    for (int x = 0; x < SCALED_WIDTH; x++)
-                    {
-                        int originalX = Mathf.RoundToInt(x * widthScaleFactor);
-                        int originalY = Mathf.RoundToInt(y * heightScaleFactor);
-
-                        Color color = originalPortrait.GetPixel(originalX, originalY);
-
-                        filteredPortrait.SetPixel(x, y, color);
-                    }
-                }
-
-                // apply 4tone monochrome
-                Color[] pixels = filteredPortrait.GetPixels();
-                Color[] newPixels = new Color[pixels.Length];
-
-                Color black = new Color(0, 0, 0);
-                Color darkGray = new Color(0.25f, 0.25f, 0.25f);
-                Color lightGray = new Color(0.75f, 0.75f, 0.75f);
-                Color white = new Color(1, 1, 1);
-
-                float threshold = 0.25f;
-
-                for (int i = 0; i < pixels.Length; i++)
-                {
-                    float grayscale = pixels[i].grayscale;
-
-                    if (grayscale < threshold)
-                        newPixels[i] = black;
-                    else if (grayscale < 2 * threshold)
-                        newPixels[i] = darkGray;
-                    else if (grayscale < 3 * threshold)
-                        newPixels[i] = lightGray;
-                    else
-                        newPixels[i] = white;
-                }
-                filteredPortrait.SetPixels(newPixels);
-
-                // Floyd-Steinberg dithering
-                for (int y = 0; y < filteredPortrait.height; y++)
-                {
-                    for (int x = 0; x < filteredPortrait.width; x++)
-                    {
-                        Color oldColor = filteredPortrait.GetPixel(x, y);
-                        Color newColor = newPixels[y * filteredPortrait.width + x];
-                        filteredPortrait.SetPixel(x, y, newColor);
-                        Color error = oldColor - newColor;
-
-                        if (x < filteredPortrait.width - 1)
-                        {
-                            filteredPortrait.SetPixel(x + 1, y, filteredPortrait.GetPixel(x + 1, y) + error * 7 / 16f);
-                        }
-
-                        if (x > 0 && y < filteredPortrait.height - 1)
-                        {
-                            filteredPortrait.SetPixel(x - 1, y + 1, filteredPortrait.GetPixel(x - 1, y + 1) + error * 3 / 16f);
-                        }
-
-                        if (y < filteredPortrait.height - 1)
-                        {
-                            filteredPortrait.SetPixel(x, y + 1, filteredPortrait.GetPixel(x, y + 1) + error * 5 / 16f);
-                        }
-
-                        if (x < filteredPortrait.width - 1 && y < filteredPortrait.height - 1)
-                        {
-                            filteredPortrait.SetPixel(x + 1, y + 1, filteredPortrait.GetPixel(x + 1, y + 1) + error * 1 / 16f);
-                        }
-                    }
-                }
-
-                // apply a colored tint
-                Color tint = new Color(0.5f, 0.5f, 0.75f, 1f);
-                for (int y = 0; y < SCALED_HEIGHT; y++)
-                {
-                    for (int x = 0; x < SCALED_WIDTH; x++)
-                    {
-                        Color color = filteredPortrait.GetPixel(x, y);
-                        // Adjust t to control the intensity of the tint
-                        Color tintedColor = Color.Lerp(color, tint, 0.25f);
-                        filteredPortrait.SetPixel(x, y, tintedColor);
-                    }
-                }
-
-                filteredPortrait.Apply();
-
-                npcPortrait.sprite = Sprite.Create(filteredPortrait, new Rect(0, 0, filteredPortrait.width, filteredPortrait.height), Vector2.one * 0.5f);
+                Texture2D portrait = Resources.Load<Texture2D>($"Portraits/{_currentNode.NpcPortrait}");
+                portrait.filterMode = FilterMode.Point; // for pixelated look
+                npcPortrait.sprite = Sprite.Create(portrait, new Rect(0, 0, portrait.width, portrait.height), Vector2.one * 0.5f);
+                npcPortrait.color = new Color(0.5f, 0.5f, 0.75f);
             }
 
             for(int i = 0; i < _currentNode.Options.Count; i++)
